d4afb8b5849ef351d8c6fd82526896b2a33164e6
officialsahogany <officialsahogany@gmail.com>
2025-09-10 10:14:58 +0900

    Fix: Poseidon's Trident dual vortex system - vortexes now spawn on both sides when entering uncontrollable state

--- pingfighter.py
+++ pingfighter.py
@@ -5120,6 +5120,7 @@ def handle_player(keys):
     global rolling_active, rolling_timer, rolling_direction, rolling_speed
     global poseidon_dash_pending, poseidon_dash_x, poseidon_dash_y  # 포세이돈 삼지창 대시 플래그
     global rolling_stun_timer, rolling_dash_available_timer, rolling_cooldown, rolling_charges, rolling_charge_timer
+    global legendary_manager  # 전설 아이템 매니저
     global player_missile_stunned_timer, player_missile_knockback_vel, current_stage  # 스테이지 6 미사일 넉백
     global is_danger_sensor_dash  # 위험감지센서 대쉬 플래그
     global token_states  #  토큰 상태 리스트
