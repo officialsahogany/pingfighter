2c3f7869631f65fda4883f1ec21a19650cc96766
officialsahogany <officialsahogany@gmail.com>
2025-09-22 20:51:50 +0900

    Auto-save #53: Python 코드 업데이트 - 2025-09-22 20:51:50

--- ui/academy_ui.py
+++ ui/academy_ui.py
@@ -661,7 +661,7 @@ class AcademyUI:
         status_pos_y = info_y + info_height - 20
         if current_level >= max_level:
             badge = self._create_max_badge_surface((255, 215, 0))
-            self.screen.blit(badge, (info_x + 10, status_pos_y - (badge.get_height() // 2)))
+            self.screen.blit(badge, (info_x + 10, status_pos_y))
         elif can_upgrade:
             upgrade_surface = self.font_small.render("클릭하여 업그레이드", True, (100, 255, 100))
             self.screen.blit(upgrade_surface, (info_x + 10, status_pos_y))
