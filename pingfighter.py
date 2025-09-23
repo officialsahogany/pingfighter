1fe5e9d8e36364bba66b9c5d52b1f19513831938
officialsahogany <officialsahogany@gmail.com>
2025-09-23 21:38:44 +0900

    Auto-save #23: 메인 게임 로직 업데이트 - 2025-09-23 21:38:44

--- pingfighter.py
+++ pingfighter.py
@@ -3766,7 +3766,7 @@ def _draw_blacksmith_upper(surface, shield_swing=0.0, hammer_swing=0.0, walk_wav
 
     hammer_ax = right_wrist[0] + 6 - int(round(10 * hammer_raise)) + int(round(16 * hammer_drop))
 
-    grip_ratio = 0.85
+    grip_ratio = 0.5
     pivot = pygame.math.Vector2(right_wrist[0] + 6, right_wrist[1] + 4)
     swing_angle = -math.radians(110) + hammer_drop * math.radians(150)
     swing_angle += hammer_raise * math.radians(10)
