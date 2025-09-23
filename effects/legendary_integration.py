6a66a5c6aa9a5c135a249269a6a7d7fb1da12997
officialsahogany <officialsahogany@gmail.com>
2025-09-23 22:10:02 +0900

    Auto-save #45: Python 코드 업데이트 - 2025-09-23 22:10:02

--- effects/legendary_integration.py
+++ effects/legendary_integration.py
@@ -157,6 +157,7 @@ def check_and_trigger_legendary_on_pickup(item_type: str, item_data: Dict[str, A
         'chronos_clock': '크로노스의 시계',
         'excalibur_blade': '엑스칼리버',
         'ragnarok_hammer': '라그나로크 해머',
+        'sacred_laurel': '신성 월계수',
         '전설의벨트': '전설의 벨트'  # 특수 시너지 아이템
     }
     
