44bca04fb1c63b04b7e3a4bf7576734c9dd2a588
officialsahogany <officialsahogany@gmail.com>
2025-09-23 21:53:59 +0900

    Auto-save #39: 메인 게임 로직 업데이트 - 2025-09-23 21:53:59

--- pingfighter.py
+++ pingfighter.py
@@ -50412,8 +50412,16 @@ def show_item_management_menu(item_list, selected_index, item_type):
                                     gravitybelt_obtained = False
                                     items.gravitybelt_obtained = False
                                 elif removed_item["name"] == "hermes_shoes":
-                                    hermes_shoes_obtained = False
-                                    items.hermes_shoes_obtained = False
+                    hermes_shoes_obtained = False
+                    items.hermes_shoes_obtained = False
+                    sacred_laurel_obtained = False
+                    items.sacred_laurel_obtained = False
+                    try:
+                        legendary_manager = get_legendary_manager()
+                        if legendary_manager:
+                            legendary_manager.deactivate_item("sacred_laurel")
+                    except Exception:
+                        pass
                                 elif removed_item["name"] == "gravitybelt":
                                     gravitybelt_obtained = False
                                     items.gravitybelt_obtained = False
