11e9da13eb0ed38679d62a7f8f827d4a4cd827dd
officialsahogany <officialsahogany@gmail.com>
2025-09-25 16:56:27 +0900

    Auto-save #211: 메인 게임 로직 업데이트 - 2025-09-25 16:56:27

--- pingfighter.py
+++ pingfighter.py
@@ -3814,15 +3814,15 @@ def _draw_blacksmith_upper(
 
         target_shoulder = (
             right_shoulder[0] + 2,
-            right_shoulder[1] - 6,
+            right_shoulder[1] - 4,
         )
         target_elbow = (
             right_elbow[0] + 6,
-            right_elbow[1] - 30,
+            right_elbow[1] - 22,
         )
         target_wrist = (
             right_wrist[0] + 4,
-            right_wrist[1] - 48,
+            right_wrist[1] - 36,
         )
         right_shoulder = _blend_point(right_shoulder, target_shoulder)
         right_elbow = _blend_point(right_elbow, target_elbow)
@@ -3842,8 +3842,8 @@ def _draw_blacksmith_upper(
     hand_center = (right_wrist[0] + 6, right_wrist[1] + 4)
     if umbrella_pose_active:
         hand_center = (
-            right_wrist[0] + 2,
-            right_wrist[1] - int(round(2 + 8 * umbrella_raise)),
+            right_wrist[0] + 1,
+            right_wrist[1] - int(round(1 + 4 * umbrella_raise)),
         )
     pygame.draw.circle(surface, (205, 185, 155), hand_center, 6)
     if umbrella_pose_active:
@@ -3862,7 +3862,7 @@ def _draw_blacksmith_upper(
         grip_ratio = 0.35
     pivot = pygame.math.Vector2(hand_center[0], hand_center[1])
     if umbrella_pose_active:
-        pivot = pygame.math.Vector2(hand_center[0], hand_center[1] - 2)
+        pivot = pygame.math.Vector2(hand_center[0], hand_center[1] - 1)
     swing_angle = -math.radians(110) + hammer_drop * math.radians(150)
     swing_angle += hammer_raise * math.radians(10)
     if umbrella_pose_active:
@@ -3875,7 +3875,7 @@ def _draw_blacksmith_upper(
 
     handle_length = 46
     if umbrella_pose_active:
-        handle_length = 70
+        handle_length = 64
     handle_half_width = 5
     if umbrella_pose_active:
         handle_half_width = 4
