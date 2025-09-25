5876ddb08f9b412be2130dbebe830949814ce11d
officialsahogany <officialsahogany@gmail.com>
2025-09-25 14:57:50 +0900

    Auto-save #163: 메인 게임 로직 업데이트 - 2025-09-25 14:57:50

--- pingfighter.py
+++ pingfighter.py
@@ -18000,6 +18000,14 @@ def handle_player(keys):
         umbrella_half_left = max(umbrella_half_left, left_extent_scaled)
         umbrella_half_right = max(umbrella_half_right, right_extent_scaled)
 
+        extra_height = int(round((left_extent_scaled + right_extent_scaled) * 0.2))
+        if extra_height > 0:
+            player_collision_rect.inflate_ip(0, extra_height)
+
+        extra_height = int(round((left_extent_scaled + right_extent_scaled) * 0.2))
+        if extra_height > 0:
+            player_collision_rect.inflate_ip(0, extra_height)
+
         expanded_width = int(math.ceil(umbrella_half_left + umbrella_half_right) * 2)
         if expanded_width > player_collision_rect.width:
             player_collision_rect.width = expanded_width
