14e53f2df2c31afaf8d95ad24a52cf6640c8c70c
officialsahogany <officialsahogany@gmail.com>
2025-09-25 09:19:05 +0900

    Auto-save #41: Python 코드 업데이트 - 2025-09-25 09:19:05

--- legendary_items.py
+++ legendary_items.py
@@ -48,19 +48,19 @@ _STANDARD_KNOCKBACK_THRESHOLDS: Tuple[Tuple[float, float], ...] = (
 
 # 공통 넉백 알고리즘 파라미터 (라그나로크 해머가 최장 거리 유지)
 KNOCKBACK_PROFILES: Dict[str, KnockbackProfile] = {
-    "bazooka": KnockbackProfile(  # 가장 짧은 넉백 (30% 하향)
-        base_power=23.8,
+    "bazooka": KnockbackProfile(  # 가장 짧은 넉백
+        base_power=34.0,
         speed_weight=0.0,
         speed_thresholds=_STANDARD_KNOCKBACK_THRESHOLDS,
         random_bonus=(0.0, 0.03),
-        max_power=30.8,
+        max_power=44.0,
     ),
-    "grenade": KnockbackProfile(  # 중간 넉백 (30% 하향)
-        base_power=26.6,
+    "grenade": KnockbackProfile(  # 중간 넉백
+        base_power=38.0,
         speed_weight=0.0,
         speed_thresholds=_STANDARD_KNOCKBACK_THRESHOLDS,
         random_bonus=(0.01, 0.04),
-        max_power=35.0,
+        max_power=50.0,
     ),
     "ragnarok": KnockbackProfile(  # 가장 긴 넉백 (30% 하향)
         base_power=30.8,
