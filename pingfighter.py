a69e19c4be28518a400cc17ade64537d6b155838
officialsahogany <officialsahogany@gmail.com>
2025-09-27 01:19:40 +0900

    Auto-save #178: 메인 게임 로직 업데이트 - 2025-09-27 01:19:40

--- pingfighter.py
+++ pingfighter.py
@@ -17524,7 +17524,6 @@ def handle_player(keys):
             return current_value + min(delta, max_step)
         return current_value + max(delta, -max_step)
 
-    umbrella_turn_blocked = False
     # === 롱부스트 타이머 체크 및 점진적 크기 변화 ===
     if long_boost_active:
         if long_boost_timer > 0:
