51cb81f28f903d1d4b6a07ae75cc6df42638b454
officialsahogany <officialsahogany@gmail.com>
2025-09-26 23:44:26 +0900

    Auto-save #142: 메인 게임 로직 업데이트 - 2025-09-26 23:44:26

--- pingfighter.py
+++ pingfighter.py
@@ -5013,6 +5013,7 @@ def handle_blacksmith_turret_input(down_pressed, down_just_pressed, force_bluepr
     global blacksmith_turret_state, special_gauge, special_ready
     global is_waiting_for_serve, is_player_serve
     global blacksmith_hammer_swing_active, blacksmith_hammer_swing_phase
+    global blacksmith_trail_timer
     global blacksmith_manual_hammer_timer
     global blacksmith_turret_partial_drain, blacksmith_turret_xp_partial_drain
     global blacksmith_divine_blueprint_active, blacksmith_divine_blueprint_rect
