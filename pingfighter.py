32b96f9a437918e232c51cb66e7cd8c274cbf237
officialsahogany <officialsahogany@gmail.com>
2025-09-26 22:38:30 +0900

    Auto-save #120: 메인 게임 로직 업데이트 - 2025-09-26 22:38:30

--- pingfighter.py
+++ pingfighter.py
@@ -29710,17 +29710,17 @@ def show_start_screen():
     global blacksmith_hammer_available, blacksmith_hammer_shock_charging
     global blacksmith_hammer_shock_charge_frames, blacksmith_hammer_shock_stage
     global blacksmith_hammer_shock_cooldown_timer, blacksmith_hammer_shock_projectiles
-    global blacksmith_hammer_last_update_frame
-    global blacksmith_hammer_last_update_frame
+    global blacksmith_hammer_last_update_frame, blacksmith_hammer_last_update_ms
+    global blacksmith_hammer_projectiles_paused
     blacksmith_hammer_available = True
     blacksmith_hammer_shock_charging = False
     blacksmith_hammer_shock_charge_frames = 0
     blacksmith_hammer_shock_stage = 0
     blacksmith_hammer_shock_cooldown_timer = 0
     blacksmith_hammer_shock_projectiles.clear()
-    blacksmith_hammer_last_update_frame = globals().get("frame_counter", blacksmith_hammer_last_update_frame)
-    global blacksmith_hammer_last_update_frame
     blacksmith_hammer_last_update_frame = globals().get("frame_counter", 0)
+    blacksmith_hammer_last_update_ms = pygame.time.get_ticks()
+    blacksmith_hammer_projectiles_paused = False
     global blacksmith_hammer_shock_anchor_x, blacksmith_hammer_shock_anchor_y
     blacksmith_hammer_shock_anchor_x = 0
     blacksmith_hammer_shock_anchor_y = 0
