4d7f92dbab859e70acd09e6b0b08e8abae883386
officialsahogany <officialsahogany@gmail.com>
2025-09-26 19:52:23 +0900

    Auto-save #71: 메인 게임 로직 업데이트 - 2025-09-26 19:52:23

--- pingfighter.py
+++ pingfighter.py
@@ -4742,7 +4742,8 @@ def create_blacksmith_paddle_umbrella(progress: float) -> pygame.Surface:
         left_padding = 210  # 우산이 왼쪽으로 크게 펼쳐질 때 잘리지 않도록 여유 공간 확보
         right_padding = 60
     else:
-        left_padding = 60
+        # 오른쪽 스윙 시에도 우산이 왼쪽으로 펼쳐지므로 충분한 left_padding 필요
+        left_padding = 250  # 우산이 잘리지 않도록 충분한 공간 확보
         right_padding = 210
     final_width = base_width + left_padding + right_padding
     final_height = 620
