77b5e0ac077a83d53567d7d5a53e47adfbb275a8
officialsahogany <officialsahogany@gmail.com>
2025-09-28 02:23:18 +0900

    Auto-save #433: 메인 게임 로직 업데이트 - 2025-09-28 02:23:18

--- pingfighter.py
+++ pingfighter.py
@@ -46942,6 +46942,7 @@ def handle_ball():
                 draw_blacksmith_divine_ui(SCREEN)
                 draw_blacksmith_divine_ui(SCREEN)
                 draw_blacksmith_divine_ui(SCREEN)
+                draw_blacksmith_divine_ui(SCREEN)
             draw_score()  # 3:0 완승 보너스 메시지도 표시
             
             #  Stage 1 듀스 모드 - 타이머 기반으로 변경되어 더 이상 점수 체크하지 않음
@@ -54476,6 +54477,8 @@ def main(stage_num, new_boss_mode=False):
                                                       stage4_magnetic_active)
             if selected_character_type == "blacksmith":
                 draw_blacksmith_turret_ui(SCREEN)
+                draw_blacksmith_divine_ui(SCREEN)
+                draw_blacksmith_divine_ui(SCREEN)
         else:
             # 흔들림이 없을 때는 직접 그리기
             draw_field()
