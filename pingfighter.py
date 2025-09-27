dc6ddd0b42819e0e5d201a590b0c15d1671b2631
officialsahogany <officialsahogany@gmail.com>
2025-09-27 22:54:19 +0900

    Auto-save #363: 메인 게임 로직 업데이트 - 2025-09-27 22:54:19

--- pingfighter.py
+++ pingfighter.py
@@ -8585,6 +8585,7 @@ blacksmith_hammer_shock_charging = False
 blacksmith_hammer_last_update_frame = 0
 blacksmith_hammer_last_update_ms = 0
 blacksmith_hammer_cooldown_remainder_ms = 0.0
+blacksmith_hammer_cooldown_debug_bucket = -1
 blacksmith_hammer_projectiles_paused = False
 BLACKSMITH_PADDLE_IMG = create_blacksmith_paddle_base()
 BLACKSMITH_PADDLE_NO_HAMMER_IMG = create_blacksmith_paddle_base(include_hammer=False)
