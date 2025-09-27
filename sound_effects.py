913122ecf5e03d67dc28768306aa424e7d9d2d7a
officialsahogany <officialsahogany@gmail.com>
2025-09-27 23:20:32 +0900

    Auto-save #376: Python 코드 업데이트 - 2025-09-27 23:20:31

--- sound_effects.py
+++ sound_effects.py
@@ -40,6 +40,7 @@ SOUND_PATHS = {
     "RAGNAROK_SHOCK": "sounds/ragnarokshock.wav",
     "CONSTRUCTION": "sounds/construction.wav",
     "BLACKSMITH_UMBRELLA_SWING": "sounds/swing.wav",
+    "BLACKSMITH_UMBRELLA_BLOCK": "sounds/blocking.wav",
     "STAGE6_BOSS_HIT": "sounds/stage6bosshit.wav",
     "STAGE6_BEAM": "sounds/stage6beam.wav",
     "STAGE6_BEAM_CHARGE": "sounds/stage6beamcharge.wav",
