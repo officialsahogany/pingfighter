8bbe83a8193dd9379e0823be125dbd765d0748fd
officialsahogany <officialsahogany@gmail.com>
2025-09-28 18:03:15 +0900

    Auto-save #640: Python 코드 업데이트 - 2025-09-28 18:03:15

--- game_logic/characters/blacksmith/state.py
+++ game_logic/characters/blacksmith/state.py
@@ -22,7 +22,6 @@ class BlacksmithTurretRuntime:
     overdrive_ui_divine: bool = False
     overheat_timer: int = 0
     overheat_smoke_timer: int = 0
-    rebuild_ready: bool = False
 
 
 @dataclass
@@ -34,7 +33,6 @@ class BlacksmithDivineRuntime:
     blueprint_rect: Any = None
     build_progress: int = 0
     partial_drain: float = 0.0
-    rebuild_ready: bool = False
 
 
 @dataclass
