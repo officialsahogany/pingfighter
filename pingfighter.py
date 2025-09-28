d658b30e06431f7a2c48a467a34d7ba8b4b3048e
officialsahogany <officialsahogany@gmail.com>
2025-09-28 12:09:21 +0900

    Auto-save #512: 메인 게임 로직 업데이트 - 2025-09-28 12:09:21

--- pingfighter.py
+++ pingfighter.py
@@ -43433,12 +43433,8 @@ def reset_round():
     global short_shot_active, short_shot_timer, short_shot_vertical_timer
     global short_shot_speed, short_shot_target_vx, short_shot_target_vy, short_shot_original_speed
     global blacksmith_shield_swing_active, blacksmith_shield_swing_timer
-    global (
-        blacksmith_hammer_swing_active,
-        blacksmith_hammer_swing_phase,
-        blacksmith_manual_hammer_timer,
-        blacksmith_manual_hammer_increment,
-    )
+    global blacksmith_hammer_swing_active, blacksmith_hammer_swing_phase
+    global blacksmith_manual_hammer_timer, blacksmith_manual_hammer_increment
     # 스톱워치/스마트폰 관련 상태 초기화 (라운드 리셋 시 강제 초기화)
     global stopwatch_active, stopwatch_timer, stopwatch_recovery_timer
     global stopwatch_original_ball_vel, stopwatch_forced_upward, stopwatch_upward_lock_timer
