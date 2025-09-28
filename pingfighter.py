c47641e77eb71efb71e3cf98bf19e14ea5a73aff
officialsahogany <officialsahogany@gmail.com>
2025-09-28 11:39:34 +0900

    Auto-save #505: 메인 게임 로직 업데이트 - 2025-09-28 11:39:33

--- pingfighter.py
+++ pingfighter.py
@@ -47051,15 +47051,15 @@ def handle_ball():
                     # 충전가방 효과
                     if chargebag_obtained and not aipill_active:
                         # 캐릭터별 기본 게이지 충전량
-                        if selected_character_type == "soldier":
-                            base_gauge_gain = 50  # 코만도: 게이지 충전 50
-                        elif selected_character_type == "blacksmith":
-                            if blacksmith_umbrella_open:
-                                base_gauge_gain = 70  # 발토르 우산 활성: 게이지 충전 70
-                            else:
-                                base_gauge_gain = 30  # 발토르 기본 패들: 게이지 충전 30
-                        else:
-                            base_gauge_gain = 80  # 스매셔: 게이지 충전 80
+            if selected_character_type == "soldier":
+                base_gauge_gain = 50  # 코만도: 게이지 충전 50
+            elif selected_character_type == "blacksmith":
+                if blacksmith_umbrella_open:
+                    base_gauge_gain = get_blacksmith_umbrella_gauge_gain()  # 발토르 우산 활성: 기본 70, 디바인스톤 활성 시 100
+                else:
+                    base_gauge_gain = 30  # 발토르 기본 패들: 게이지 충전 30
+            else:
+                base_gauge_gain = 80  # 스매셔: 게이지 충전 80
                         # 충전가방은 기본 게이지 충전량의 20%만 적용 (스킬/아이템 보너스 제외)
                         chargebag_gain = int(base_gauge_gain * 0.2)  # 기본 충전량의 20%
                         old_gauge = special_gauge
@@ -48482,7 +48482,7 @@ def handle_ball():
                     base_gauge_gain = 50  # 코만도: 게이지 충전 50
                 elif selected_character_type == "blacksmith":
                     if blacksmith_umbrella_open:
-                        base_gauge_gain = 70  # 발토르 우산 활성: 게이지 충전 70
+                        base_gauge_gain = get_blacksmith_umbrella_gauge_gain()  # 발토르 우산 활성: 기본 70, 디바인스톤 활성 시 100
                     else:
                         base_gauge_gain = 30  # 발토르 기본 패들: 게이지 충전 30
                 else:
