58d158cb8c4d4c848cee03bc02fe696e70ca9796
officialsahogany <officialsahogany@gmail.com>
2025-09-30 03:07:56 +0900

    Auto-save #143: 메인 게임 로직 업데이트 - 2025-09-30 03:07:56

--- pingfighter.py
+++ pingfighter.py
@@ -54946,6 +54946,9 @@ def main(stage_num, new_boss_mode=False):
 
     boss_fail_timer = 0  # 보스 실수 타이머 초기화
     current_stage = stage_num
+    global tutorial_guide_hint_visible, tutorial_guide_hint_start_ticks
+    tutorial_guide_hint_visible = True
+    tutorial_guide_hint_start_ticks = pygame.time.get_ticks()
     global soldier_emergency_supply_stage, soldier_emergency_supply_used
     global soldier_down_tap_timer, soldier_down_tap_count, soldier_down_tap_suppress_timer
     soldier_emergency_supply_stage = stage_num
