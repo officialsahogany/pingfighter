d6d7f7f67334b10ceb044122561fc9c8655888cd
officialsahogany <officialsahogany@gmail.com>
2025-10-02 16:15:44 +0900

    Auto-save #189: 메인 게임 로직 업데이트 - 2025-10-02 16:15:44

--- pingfighter.py
+++ pingfighter.py
@@ -12845,7 +12845,6 @@ def reset_blacksmith_state(*, preserve_divine: bool = False, stage_num: int | No
     global blacksmith_blocking_penalty_timer, blacksmith_blocking_toast_timer
     global blacksmith_blocking_skill_timer
     global blacksmith_blocking_bonus_pending
-    global blacksmith_blocking_bonus_pending
     blacksmith_hammer_swing_slow_timer = 0
     stop_blacksmith_hammer_charge_sound()
     blacksmith_umbrella_gauge = BLACKSMITH_UMBRELLA_GAUGE_MAX
@@ -12857,7 +12856,6 @@ def reset_blacksmith_state(*, preserve_divine: bool = False, stage_num: int | No
     blacksmith_blocking_penalty_timer = 0
     blacksmith_blocking_toast_timer = 0
     blacksmith_blocking_skill_timer = 0
-    blacksmith_blocking_bonus_pending = False
     blacksmith_blocking_bonus_pending = False
     blacksmith_umbrella_hit_lock = False
     blacksmith_hammer_shock_last_stage = 0
