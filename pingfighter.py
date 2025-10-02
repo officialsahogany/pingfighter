03d0cf4b795cc63a0eb2bc135bb37e20efa522a0
officialsahogany <officialsahogany@gmail.com>
2025-10-02 12:14:08 +0900

    Auto-save #67: 메인 게임 로직 업데이트 - 2025-10-02 12:14:08

--- pingfighter.py
+++ pingfighter.py
@@ -35021,6 +35021,7 @@ def show_start_screen():
     global game_should_exit, smoke_zones, frame_count
     global blacksmith_build_menu_active, blacksmith_down_hold_frames, blacksmith_divine_stone_state
 
+    effects_manager.clear_all_effects()
     damage_manager = get_damage_manager()
     damage_manager.reset_all()
 
