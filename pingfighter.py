89fc6a4a67b6984073b98957ee0257543e0cce0e
officialsahogany <officialsahogany@gmail.com>
2025-10-03 14:43:48 +0900

    Auto-save #9: 메인 게임 로직 업데이트 - 2025-10-03 14:43:48

--- pingfighter.py
+++ pingfighter.py
@@ -15781,7 +15781,6 @@ def go_to_next_round():
         print("Aipill  .")
         aipill_active = False
         deactivate_doping_potion()
-        deactivate_berserk_potion()
     # speedgear_obtained = False   # 제거 - 패시브 아이템은 라운드가 바뀌어도 유지
     # items.speedboots_obtained = False  # 제거
     # items.speedgear_obtained = False   # 제거
