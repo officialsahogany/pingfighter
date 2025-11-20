91ef8ec17c0346005272e166398b568083f5dc6b
officialsahogany <officialsahogany@gmail.com>
2025-11-20 15:10:45 +0900

    Auto-save #227: 메인 게임 로직 작업 - 2025-11-20 15:10:45

--- pingfighter.py
+++ pingfighter.py
@@ -19050,6 +19050,16 @@ def update_suicide_drone(keys, manual_trigger_edge: bool, mouse_pressed_edge: bo
             return
     except Exception:
         pass
+
+    # 보스 뒷벽 라인 도달 시 폭발 (공 실점 라인과 동일 Y 기준)
+    try:
+        boss_back_wall_y = BOSS.bottom + 20  # 보스 뒤쪽 패들-벽 사이 여유
+        if globals().get('suicide_drone_grace_timer', 0) <= 0 and suicide_drone_rect.top <= boss_back_wall_y:
+            if suicide_drone_rect.centerx >= BOSS.left and suicide_drone_rect.centerx <= BOSS.right:
+                _detonate_suicide_drone("boss_back_wall")
+                return
+    except Exception:
+        pass
     
 
 
