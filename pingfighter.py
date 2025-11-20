c4aaa0f9296145a0cca5275edc20fe9e2c98724c
officialsahogany <officialsahogany@gmail.com>
2025-11-20 16:30:33 +0900

    Auto-save #259: 메인 게임 로직 작업 - 2025-11-20 16:30:33

--- pingfighter.py
+++ pingfighter.py
@@ -40335,7 +40335,7 @@ def draw_objects():
                     arrow_spacing = 50
                     current_phase = (ticks // 150) % 2  # 150ms마다 좌우 전환 (더 빠름)
 
-                    # 왼쪽 화살표 (←)
+                    # 왼쪽 화살표 (←) - 하나의 연결된 도형
                     left_x = base_x - arrow_spacing // 2 + shake_x
                     left_y = base_y + shake_y
                     left_scale = 1.5 if current_phase == 0 else 0.8  # 더 큰 크기 변화
@@ -40348,40 +40348,33 @@ def draw_objects():
                     # 강렬한 화살표 색상 (빨강-노랑 그라데이션)
                     arrow_color = (255, 50 + int(150 * fast_pulse), 0)  # 빨강에서 주황으로
 
-                    # 화살촉 (삼각형) - 왼쪽
+                    # 화살표 전체를 하나의 도형으로 정의 (시계방향)
                     arrow_head_width = scaled_size * 0.6
-                    arrow_head = [
-                        (0, scaled_size * 0.5),  # 왼쪽 끝 (화살촉 끝)
-                        (arrow_head_width, scaled_size * 0.2),  # 위쪽 꼭지점
-                        (arrow_head_width, scaled_size * 0.8),  # 아래쪽 꼭지점
-                    ]
-
-                    # 화살대 (사각형) - 오른쪽
                     shaft_start_x = arrow_head_width * 0.7
                     shaft_width = scaled_size * 0.8
                     shaft_height = scaled_size * 0.3
-                    arrow_shaft = [
-                        (shaft_start_x, scaled_size * 0.5 - shaft_height / 2),
-                        (shaft_start_x + shaft_width, scaled_size * 0.5 - shaft_height / 2),
-                        (shaft_start_x + shaft_width, scaled_size * 0.5 + shaft_height / 2),
-                        (shaft_start_x, scaled_size * 0.5 + shaft_height / 2),
+
+                    left_arrow_points = [
+                        (0, scaled_size * 0.5),  # 1. 화살촉 끝 (왼쪽)
+                        (arrow_head_width, scaled_size * 0.2),  # 2. 화살촉 위쪽
+                        (arrow_head_width, scaled_size * 0.5 - shaft_height / 2),  # 3. 화살대 위쪽 왼쪽
+                        (shaft_start_x + shaft_width, scaled_size * 0.5 - shaft_height / 2),  # 4. 화살대 위쪽 오른쪽
+                        (shaft_start_x + shaft_width, scaled_size * 0.5 + shaft_height / 2),  # 5. 화살대 아래쪽 오른쪽
+                        (arrow_head_width, scaled_size * 0.5 + shaft_height / 2),  # 6. 화살대 아래쪽 왼쪽
+                        (arrow_head_width, scaled_size * 0.8),  # 7. 화살촉 아래쪽
                     ]
 
                     # 그림자 효과
                     shadow_offset = 3
-                    shadow_head = [(x + shadow_offset, y + shadow_offset) for x, y in arrow_head]
-                    shadow_shaft = [(x + shadow_offset, y + shadow_offset) for x, y in arrow_shaft]
-                    pygame.draw.polygon(left_arrow_surf, (0, 0, 0, int(150 * fade_alpha)), shadow_head)
-                    pygame.draw.polygon(left_arrow_surf, (0, 0, 0, int(150 * fade_alpha)), shadow_shaft)
+                    shadow_points = [(x + shadow_offset, y + shadow_offset) for x, y in left_arrow_points]
+                    pygame.draw.polygon(left_arrow_surf, (0, 0, 0, int(150 * fade_alpha)), shadow_points)
 
-                    # 화살표 본체
-                    pygame.draw.polygon(left_arrow_surf, (*arrow_color, left_alpha), arrow_head)
-                    pygame.draw.polygon(left_arrow_surf, (*arrow_color, left_alpha), arrow_shaft)
+                    # 화살표 본체 (하나의 도형)
+                    pygame.draw.polygon(left_arrow_surf, (*arrow_color, left_alpha), left_arrow_points)
 
-                    # 강렬한 하이라이트
+                    # 강렬한 하이라이트 (하나의 외곽선)
                     highlight_alpha = min(255, int((left_alpha + 80) * fade_alpha))
-                    pygame.draw.polygon(left_arrow_surf, (255, 255, 255, highlight_alpha), arrow_head, 4)
-                    pygame.draw.polygon(left_arrow_surf, (255, 255, 255, highlight_alpha), arrow_shaft, 4)
+                    pygame.draw.polygon(left_arrow_surf, (255, 255, 255, highlight_alpha), left_arrow_points, 4)
 
                     SCREEN.blit(left_arrow_surf, (left_x - int(scaled_size * 0.75), left_y - scaled_size // 2))
 
