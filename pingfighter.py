6f081556ce92ceef8723fc3818ff11e9e5bf11a5
officialsahogany <officialsahogany@gmail.com>
2025-11-21 23:50:01 +0900

    Auto-save #377: 메인 게임 로직 작업 - 2025-11-21 23:50:01

--- pingfighter.py
+++ pingfighter.py
@@ -39373,6 +39373,9 @@ def draw_objects():
     elif current_stage == 5:
         boss_img = BOSS_IMG_STAGE5
         boss_w, boss_h = BOSS_IMG_STAGE5_WIDTH, BOSS_IMG_STAGE5_HEIGHT
+    elif current_stage == 8:
+        boss_img = BOSS_IMG_STAGE8
+        boss_w, boss_h = BOSS_IMG_STAGE8_WIDTH, BOSS_IMG_STAGE8_HEIGHT
     elif current_stage == 7:
         boss_img_prescaled = True
         global stage7_prev_x, stage7_lean_value
@@ -39440,13 +39443,6 @@ def draw_objects():
         boss_w, boss_h = BOSS_IMG_WIDTH, BOSS_IMG_HEIGHT
     if not boss_img_prescaled:
         boss_img = pygame.transform.scale(boss_img, (boss_w, boss_h))
-    # Stage 8 idle bobbing animation
-    if current_stage == 8:
-        frames = BOSS_IMG_STAGE8_FRAMES if BOSS_IMG_STAGE8_FRAMES else [BOSS_IMG_STAGE8]
-        idx = (pygame.time.get_ticks() // 120) % len(frames)
-        boss_img = frames[idx]
-        boss_w, boss_h = boss_img.get_width(), boss_img.get_height()
-        boss_img_prescaled = True
     if current_stage != 7:
         stage7_prev_x = None
         stage7_lean_value *= 0.85
