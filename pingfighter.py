45c0610abe36d8f72edd7e875fed7918e65ea0d1
officialsahogany <officialsahogany@gmail.com>
2025-11-30 02:06:48 +0900

    Auto-save #62: 메인 게임 로직 작업 - 2025-11-30 02:06:48

--- pingfighter.py
+++ pingfighter.py
@@ -84163,7 +84163,7 @@ def show_stage_selection(show_character_hint=True):
         else:
             card_width = 160  # 아이템 관리자 전용 축소 크기
             card_height = 120
-        carㅁds_per_row = 3
+        cards_per_row = 3
         card_spacing = 40
         start_y = 180
         
