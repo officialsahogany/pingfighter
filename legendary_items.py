c76bdfe38356dc485dee0ddf8f47da5dd7cda07a
officialsahogany <officialsahogany@gmail.com>
2025-11-30 03:04:39 +0900

    Auto-save #86: Python 코드 작업 - 2025-11-30 03:04:38

--- legendary_items.py
+++ legendary_items.py
@@ -2636,6 +2636,7 @@ class AngelBlessing(LegendaryItem):
         self.roll_face = 1
         self.roll_anim_active = False
         self.roll_anim_duration = 1.1
+        self.waiting_for_space = False  # 스페이스바 대기 상태
         self._frame_cache: Dict[int, pygame.Surface] = {}
 
         # 라그나로크 해머와 동일한 애니메이션 프레임 구조
@@ -2854,6 +2855,7 @@ class AngelBlessing(LegendaryItem):
         self.roll_face = dice_face
         self.roll_anim_active = True
         self.roll_timer = 0.0
+        self.waiting_for_space = False  # 스페이스바 대기 상태 초기화
         # 버프 적용
         for buff in selected:
             self._apply_buff(buff)
