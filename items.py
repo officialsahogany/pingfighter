755311b2d276dfdbab75e8c5290ca7fc2c936c1b
officialsahogany <officialsahogany@gmail.com>
2025-11-30 05:43:43 +0900

    Auto-save #169: Python 코드 작업 - 2025-11-30 05:43:43

--- items.py
+++ items.py
@@ -438,10 +438,12 @@ def load_item_icons():
         "repair_kit": "repair_kit.png",  # 수리키트 아이콘
         "vitamin_pill": "vitamin_pill.png",  # 비타민약 아이콘 (없을 시 코드로 그립니다)
         "laser_scope": "laser_scope.png",  # 레이저스코프 아이콘
+        "knee_pads": "knee_pads.png",  # 무릎보호대 아이콘
         # 전설 아이템(아이콘)
         "ragnarok_hammer": "legendary/ragnarok_hammer.png",
         "hermes_shoes": "legendary/hermes_shoes.png",
-        "poseidon_trident": "legendary/poseidon_trident.png"
+        "poseidon_trident": "legendary/poseidon_trident.png",
+        "angel_blessing": "legendary/angel_blessing.png"  # 천사의 가호 아이콘
     }
 
     legendary_manager = None
