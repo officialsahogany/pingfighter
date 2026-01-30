c6a5a1341e9ba9792fc58baa64b75728aa7ad2b6
officialsahogany <officialsahogany@gmail.com>
2026-01-31 03:48:37 +0900

    Fix: DASH_DELAY 사운드 경로 추가

--- sound_effects.py
+++ sound_effects.py
@@ -87,6 +87,7 @@ SOUND_PATHS = {
     "BUTTON_CLICK": "sounds/button_click.wav",
     "BUTTON_HOVER": "sounds/button_hover.wav",
     "DASH_CHARGE": "sounds/dashcharge.wav",
+    "DASH_DELAY": "sounds/dashdelay.wav",
     "THROWING_BANANA": "sounds/throwingbanana.wav",
     "STEP_BANANA": "sounds/bananastep.wav",
     "STAGE5_WARNING": "sounds/stage5warning.wav",
