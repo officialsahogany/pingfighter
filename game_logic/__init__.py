6400c59844391e8c9a9a5c4e5e14c6ba63f070c0
officialsahogany <officialsahogany@gmail.com>
2026-01-31 04:26:04 +0900

    Fix: game_logic/__init__.py 복구

--- game_logic/__init__.py
+++ game_logic/__init__.py
@@ -1,19 +1,27 @@
-"""발토르(Blacksmith) 캐릭터 전용 모듈."""
+"""
+게임 로직 모듈
+게임의 핵심 로직을 관리하는 모듈들
+"""
 
-from .state import (
-    BlacksmithState,
-    BlacksmithTurretRuntime,
-    BlacksmithDivineRuntime,
-    BlacksmithConstructionAudio,
-    blacksmith_state,
-)
-from .controller import BlacksmithController
+from .collision_manager import CollisionManager
+from .score_manager import ScoreManager
+from .round_manager import RoundManager
+from .physics_manager import PhysicsManager
+from .boss_ai import BossAI
+from .skill_system import SkillSystem
+from .item_system import ItemSystem
+from .game_loop import GameLoop, LegacyHooks, create_game_loop, create_game_vars_adapters
 
 __all__ = [
-    "BlacksmithState",
-    "BlacksmithTurretRuntime",
-    "BlacksmithDivineRuntime",
-    "BlacksmithConstructionAudio",
-    "blacksmith_state",
-    "BlacksmithController",
+    'CollisionManager', 
+    'ScoreManager', 
+    'RoundManager', 
+    'PhysicsManager', 
+    'BossAI',
+    'SkillSystem',
+    'ItemSystem',
+    'GameLoop',
+    'create_game_loop',
+    'create_game_vars_adapters',
+    'LegacyHooks'
 ]
