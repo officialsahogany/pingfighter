4e11215106030441a40d8755f00dbdfc217bcedd
officialsahogany <officialsahogany@gmail.com>
2026-06-17 11:19:12 +0900

    Use character runtime fallback in effects frame context

--- godot/scripts/core/battle_update_effects_frame_context_builder.gd
+++ godot/scripts/core/battle_update_effects_frame_context_builder.gd
@@ -4,6 +4,7 @@ const BattleSceneOwnerReader := preload("res://scripts/core/battle_scene_owner_r
 const BattleUpdateDashSnapshotBuilder := preload("res://scripts/core/battle_update_dash_snapshot_builder.gd")
 const BattleUpdateEffectsOwnerContextBuilder := preload("res://scripts/core/battle_update_effects_owner_context_builder.gd")
 const BattleUpdateEffectsSpriteContextBuilder := preload("res://scripts/core/battle_update_effects_sprite_context_builder.gd")
+const PlayerCharacterRuntime := preload("res://scripts/characters/player_character_runtime.gd")
 
 var dash_snapshot_builder: Object = BattleUpdateDashSnapshotBuilder.new()
 var owner_context_builder: Object = BattleUpdateEffectsOwnerContextBuilder.new()
@@ -13,9 +14,9 @@ var sprite_context_builder: Object = BattleUpdateEffectsSpriteContextBuilder.new
 func build_context(owner: Object, registry: Object) -> Dictionary:
 	var sprite_context: Dictionary = sprite_context_builder.build_context(
 		BattleSceneOwnerReader.get_dictionary(owner, "battle_textures"),
-		BattleSceneOwnerReader.get_value(owner, "selected_character_type", "smasher")
+		BattleSceneOwnerReader.get_value(owner, "selected_character_type", PlayerCharacterRuntime.SMASHER)
 	)
-	var character_type: String = str(sprite_context.get("selected_character_type", "smasher"))
+	var character_type: String = str(sprite_context.get("selected_character_type", PlayerCharacterRuntime.SMASHER))
 	var context: Dictionary = owner_context_builder.build_context(
 		owner,
 		registry,
